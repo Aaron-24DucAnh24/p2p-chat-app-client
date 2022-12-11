5d0877f5320c7a8cf8214b0f33f716e373eb89ef
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2022-12-11 11:24:07 +0700

    Complete MVP 1st

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,11 @@ var msgBox      = document.querySelector('.msg-container')
 var fileBox     = document.querySelector('.file-note')
 var friendList  = document.querySelector('.friend-list')
 var emojis      = document.querySelectorAll('.emoji-item')
+var msgValue  = ''
+var fileValue = []
 var curPeerName = ''
+var peerIPs
+var peerMsgs = []
 
 // log out
 logoutBtn.addEventListener('click', (event) => {
@@ -17,7 +21,6 @@ logoutBtn.addEventListener('click', (event) => {
 })
 
 // get text message value
-var msgValue  = ''
 msg.oninput = () => {msgValue = msg.value}
 
 // fill emoji to chat box
@@ -27,7 +30,6 @@ function fillEmoji(code) {
 }
 
 // handle file box
-var fileValue = []
 file.onchange = () => {
     fileValue = file.files
     if(fileValue.length == 0) {
@@ -43,13 +45,9 @@ sendBtn.addEventListener('click', async (event) => {
 
     // text message
     if(msgValue) {
-        chatBox.innerHTML = chatBox.innerHTML + 
-        `<div class="message message--me">
-            <p class="meta">Me</p>
-            <p class="text">
-                ${msgValue}
-            </p>
-        </div>` ;
+        var html = `<div class="message message--me"><p class="meta">Me</p><p class="text">${msgValue}</p></div>`
+        chatBox.innerHTML += html
+        setPeerMsg(curPeerName, html)
         window.appAPI.sendTextTrunk({name: '', text: msgValue})
         msg.value = ''
         msgValue  = ''
@@ -63,29 +61,25 @@ sendBtn.addEventListener('click', async (event) => {
             reader.readAsDataURL(fileValue[0])
             reader.onload = function (e) {
                 var src = e.target.result
-                chatBox.innerHTML = chatBox.innerHTML + 
-                `<div class="me img-message">
-                    <img src="${src}" class="img">
-                    <a href="${src}" class="download-me clickable" download><i class="fa-solid fa-download"></i></a>
-                </div>`;
-                msgBox.scrollTop = msgBox.scrollHeight
+                var html = `<div class="me img-message"><img src="${src}" class="img"><a href="${src}" class="download-me clickable" download><i class="fa-solid fa-download"></i></a></div>`
+                chatBox.innerHTML += html
+                setPeerMsg(curPeerName, html)
                 window.appAPI.sendImgTrunk({name: '', img: src})
             }
             fileValue = []
             fileBox.innerHTML = 'No selected file'
         }
+
+        // other files message
         else {
             var reader = new FileReader()
             var name = fileValue[0].name
             reader.readAsDataURL(fileValue[0])
             reader.onload = function (e) {
                 var src = e.target.result
-                chatBox.innerHTML = chatBox.innerHTML + 
-                `<div class="me file-message">
-                    ${name}
-                    <a href="${src} "class="download-me clickable" download><i class="fa-solid fa-download"></i></a>
-                </div>`
-                msgBox.scrollTop = msgBox.scrollHeight
+                var html = `<div class="me file-message">${name}<a href="${src} "class="download-me clickable" download><i class="fa-solid fa-download"></i></a></div>`
+                chatBox.innerHTML += html
+                setPeerMsg(curPeerName, html)
                 window.appAPI.sendFileTrunk({name: '', fileName: name, file: src})
             }
             fileValue = []
@@ -98,71 +92,108 @@ sendBtn.addEventListener('click', async (event) => {
 
 // get text message handler
 window.appAPI.getTextTrunk((event, textTrunk) => {
-    if(textTrunk.name == curPeerName) {
-        chatBox.innerHTML = chatBox.innerHTML + 
+    var html = 
         `<div class="message">
             <p class="meta">${textTrunk.name}</p>
-            <p class="text">
-                ${textTrunk.text}
-            </p>
-        </div>` ;
+            <p class="text">${textTrunk.text}</p>
+        </div>`;
+
+    if(textTrunk.name == curPeerName) {
+        chatBox.innerHTML += html
         msgBox.scrollTop = msgBox.scrollHeight
     }
+
+    setPeerMsg(textTrunk.name, html)
+    if(textTrunk.name != curPeerName)
+        document.querySelector('#' + textTrunk.name).classList.add('unread')
 })
 
 // get img message handler
 window.appAPI.getImgTrunk((event, imgTrunk) => {
-    if(imgTrunk.name == curPeerName){
-        chatBox.innerHTML = chatBox.innerHTML +
+    var html = 
         `<div class="img-message">
             <img src="${imgTrunk.img}" class="img">
             <a href="${imgTrunk.img}"class="download clickable"><i class="fa-solid fa-download"></i></a>
         </div>`;
+
+    if(imgTrunk.name == curPeerName) {
+        chatBox.innerHTML += html
+        setTimeout(() => {msgBox.scrollTop = msgBox.scrollHeight},20);
     }
-    setTimeout(() => {msgBox.scrollTop = msgBox.scrollHeight},20);
+
+    setPeerMsg(imgTrunk.name, html)
+    if(imgTrunk.name != curPeerName)
+        document.querySelector('#' + imgTrunk.name).classList.add('unread')
 })
 
 // get file message handler
 window.appAPI.getFileTrunk((event, fileTrunk) => {
-    if(fileTrunk.name == curPeerName){
-        chatBox.innerHTML = chatBox.innerHTML +
-        `<div class="file-message">
-            ${fileTrunk.fileName}
+    var html = 
+        `<div class="file-message"> ${fileTrunk.fileName}
             <a href="${fileTrunk.file} "class="download clickable" download><i class="fa-solid fa-download"></i></a>
         </div>`;
+
+    if(fileTrunk.name == curPeerName) {
+        chatBox.innerHTML += html
+        setTimeout(() => {msgBox.scrollTop = msgBox.scrollHeight},20);
     }
-    setTimeout(() => {msgBox.scrollTop = msgBox.scrollHeight},20);
+
+    setPeerMsg(fileTrunk.name, html)
+    if(fileTrunk.name != curPeerName)
+        document.querySelector('#' + fileTrunk.name).classList.add('unread')
 })
 
-// Handle getting IP address and switching peers
-var peerIPs
+// handle getting IP address
+var count = 0
 window.appAPI.getIP((event, IPs) => {
-    var html = ''
     peerIPs = IPs
 
-    for(var IP of IPs) {
-        if(IP.name !== curPeerName)
-            html += 
-            `<li>
-                <a class="friend" id="${IP.name}" onclick="switchPeer(this.id)">
-                    ${IP.name}
-                </a>
-            </li>`
-        else 
-            html += 
-            `<li>
-                <a class="friend focused" id="${IP.name}" onclick="switchPeer(this.id)">
-                    ${IP.name}
-                </a>
-            </li>`
-    }
-        
-    if(html !== friendList.innerHTML){ 
-        friendList.innerHTML = html
-        html = ''
+    if(!count) {
+        for(var IP of IPs) {
+            if(IP.ip) 
+                friendList.innerHTML += `<li><a class="friend online" id="${IP.name}" onclick="switchPeer(this.id)">${IP.name}</a></li>`
+            else
+                friendList.innerHTML += `<li><a class="friend" id="${IP.name}" onclick="switchPeer(this.id)">${IP.name}</a></li>`
+            if(!containPeer(IP.name)) peerMsgs.push({name: IP.name, html: ''})
+        }
+        switchPeer(IPs[0].name)
+        count = 1
+    } 
+
+    else {
+        for(var IP of IPs) {
+            if(!containPeer(IP.name)) {
+                peerMsgs.push({name: IP.name, html: ''})
+                friendList.innerHTML += `<li><a class="friend" id="${IP.name}" onclick="switchPeer(this.id)">${IP.name}</a></li>`
+            }
+            if(!IP.ip)
+                document.querySelector('#' + IP.name).classList.remove('online')
+            else if(!document.querySelector('#' + IP.name).classList.contains('online'))
+                document.querySelector('#' + IP.name).classList.add('online')
+        }
     }
 })
 
+function containPeer(peerName) {
+    for(var peerMsg of peerMsgs)
+        if(peerMsg.name === peerName)
+            return true
+    return false
+}
+
+function setPeerMsg(peerName, html) {
+    for(var peerMsg of peerMsgs)
+        if(peerMsg.name === peerName)
+            peerMsg.html += html
+}
+
+function getPeerMsg(peerName) {
+    for(var peerMsg of peerMsgs)
+        if(peerMsg.name == peerName)
+            return peerMsg.html
+}
+
+// switching peers
 function switchPeer(Name) {
     if(curPeerName !== Name) {
         if(curPeerName)
@@ -174,7 +205,9 @@ function switchPeer(Name) {
                 break 
             }
         }
-        chatBox.innerHTML = ''
+        chatBox.innerHTML = getPeerMsg(curPeerName)
+        msgBox.scrollTop = msgBox.scrollHeight
         document.querySelector('#' + Name).classList.add('focused')
+        document.querySelector('#' + Name).classList.remove('unread')
     }
 }
